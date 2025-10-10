6013add4b5faee7f87fc3fc4f9c4ff4fb81973b7
pixalivetech <pixalivetech@gmail.com>
2025-10-10 16:38:58 +0530

    Update Landings.jsx

--- src/Pages/Landings.jsx
+++ src/Pages/Landings.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Hero from "../Components/Landing/Hero";
-import About from "../Components/Landing/About"
+import Hero from "../Components/Landing/hero";
+import About from "../Components/Landing/about"
 import University from "../Components/Landing/university"
-import Location from "../Components/Landing/Location"
-import Journey from "../Components/Landing/Journey"
-import Logos from "../Components/Landing/Logos"
-import Program from "../Components/Landing/Program"
-import Leadhero from "../Components/Landing/Leadhero"
-import Test from "../Components/Landing/Testimonials"
-import Contact from "../Components/Landing/Contact"
-import Blog from "../Components/Landing/Blog"
-import Subscribe from "../Components/Landing/Subscribe"
+import Location from "../Components/Landing/location"
+import Journey from "../Components/Landing/journey"
+import Logos from "../Components/Landing/logos"
+import Program from "../Components/Landing/program"
+import Leadhero from "../Components/Landing/leadhero"
+import Test from "../Components/Landing/testimonials"
+import Contact from "../Components/Landing/contact"
+import Blog from "../Components/Landing/blog"
+import Subscribe from "../Components/Landing/subscribe"
 
 
 function App() {
